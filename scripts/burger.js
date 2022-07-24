d7feca716a2f64a342dfff5a0bbc120a1006fac4
Allxy <allxy01@gmail.com>
2022-07-24 18:25:48 +0300

    added burger

---
+++ scripts/burger.js
@@ -0,0 +1,7 @@
+document.getElementById("burgerButton").addEventListener("click", (e) => {
+  document.getElementById("header").classList.toggle("header_active");
+});
+
+document.getElementById("closeBurgeButton").addEventListener("click", (e) => {
+  document.getElementById("header").classList.toggle("header_active");
+});
