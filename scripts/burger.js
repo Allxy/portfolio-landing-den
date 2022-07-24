60cc6256edfcbd2b3527ee4267ff8f7bbca7eae6
Allxy <allxy01@gmail.com>
2022-07-24 18:37:43 +0300

    fix burger event for links

--- scripts/burger.js
+++ scripts/burger.js
@@ -2,6 +2,12 @@ document.getElementById("burgerButton").addEventListener("click", (e) => {
   document.getElementById("header").classList.toggle("header_active");
 });
 
-document.getElementById("closeBurgeButton").addEventListener("click", (e) => {
-  document.getElementById("header").classList.toggle("header_active");
+document.getElementById("header").addEventListener("click", (e) => {
+  console.log(e.target);
+  if (
+    e.target.classList.contains("header__link") ||
+    e.target.id === "closeBurgeButton"
+  ) {
+    document.getElementById("header").classList.remove("header_active");
+  }
 });
