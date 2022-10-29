f829cf69e98d6aac4074f1f77be7415264849eea
Allxy <allxy01@gmail.com>
2022-10-29 11:00:22 +0300

    feat: webpack and mixins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import "./index.scss"
+
+
 document.getElementById("burgerButton").addEventListener("click", (e) => {
   document.getElementById("header").classList.toggle("header_active");
 });
 
 document.getElementById("header").addEventListener("click", (e) => {
-  console.log(e.target);
   if (
     e.target.classList.contains("header__link") ||
     e.target.id === "closeBurgeButton"
